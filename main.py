0f2d7a4143c6e7eb601964a2e189a603ee6cfbe9
JuneSunAt7 <jprogram@bk.ru>
2023-04-14 06:03:26 +0300

    NEW DESIGN

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.addButton.clicked.connect(self.add_task)
+        self.frame.setVisible(False)
+        self.archiveData.setVisible(False)
+        self.fireTasksFrame.setVisible(False)
+        self.weekFrame.setVisible(False)
 
     def add_task(self):
-        self.tableWidget.hide()
+        self.tableWidget.setVisible(False)
+        self.frame.setVisible(True)
 
 
 def main():
