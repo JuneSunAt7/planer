ee024c96e67e27f7fd1003f93979cfe50b55d333
JuneSunAt7 <jprogram@bk.ru>
2023-04-18 10:51:11 +0300

    DATE MANAGMENT

--- filemanagment.py
+++ filemanagment.py
@@ -4,27 +4,41 @@ from PyQt5 import QtWidgets  # for message about errors
 
 
 def read_curr():
+    s = ''
+    with open('file.csv', newline='') as csvfile:
+        for line in csvfile:
+            columns = line.split(',')
+            s += str(columns[0]+' '+columns[1]+' '+columns[2]+' '+columns[3]).replace('"', '')
+    s = s.replace("task deadline diff resources", '')
+    parse = s.strip('[]').replace("\r\n", "").split(' ')
+    print(parse)
 
-    with open('tasks.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row['task'], row['deadline'], row['diff'], row['resources'])
+    return parse
 
 
 def read_fired():
     pass
 
 
-def rm_to_fired():
+def read_arc():
     pass
 
 
-def rm_to_archive():
+def m_to_fired():
+    pass
+
+
+def m_to_archive():
+    pass
+
+
+def wr_to_main_file():
     pass
 
 
 def delete():
     pass
+
 
 def rm_all():
     pass
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-import os
 from PyQt5 import QtWidgets, uic
 import design
 import filemanagment
-
+from PyQt5.QtWidgets import QTableWidgetItem
+from datetime import datetime
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
@@ -21,13 +21,22 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.fireTasksFrame.setVisible(False)
         self.weekFrame.setVisible(False)
 
+        self.tableWidget.setItem(0, 0, QTableWidgetItem(filemanagment.read_curr()[0]))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem(filemanagment.read_curr()[1]))
+        self.tableWidget.setItem(0,3, QTableWidgetItem(filemanagment.read_curr()[2]))
+
+        date = datetime.strptime(filemanagment.read_curr()[1], "%d.%m.%Y").date()
+        today = datetime.today().date()
+        print(today)
+        self.tableWidget.setItem(0, 2, QTableWidgetItem(str(date)))
+        self.tableWidget.setItem(0, 4, QTableWidgetItem(filemanagment.read_curr()[3]))
+
     def __clear(self):
         self.frame.setVisible(False)
         self.archiveData.setVisible(False)
         self.fireTasksFrame.setVisible(False)
         self.weekFrame.setVisible(False)
         self.tableWidget.setVisible(False)
-
 
     def add_task(self):
         self.__clear()
@@ -50,10 +59,8 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.archiveData.setVisible(True)
 
 
-
-
-
 def main():
+    filemanagment.read_curr()
     app = QtWidgets.QApplication(sys.argv)
     window = ExampleApp()
     window.show()
